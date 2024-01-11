4d871aea93ff90d05be4d8e5c9317fc607304477
Perparim Ciriku <155301355+PerparimCiriku@users.noreply.github.com>
2024-01-11 20:28:50 +0100

    Add files via upload

--- aboutScript.js
+++ aboutScript.js
@@ -20,3 +20,32 @@ function checkVideoAvailability(videoURL) {
   // Example usage
   const videoURL = document.getElementById("video").src;
   checkVideoAvailability(videoURL);
+
+  
+  const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.opacity = 100;
+    } else {
+      slide.style.opacity = 0;
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+
+  
